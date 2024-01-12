9499eb3f9df360084c31444d2460e1b1648a62b4
Nils Blessing <nb@2av.de>
2024-01-12 15:23:16 +0100

    adapted class to be non-generic

--- Assets/Helpers/Multilanguage.cs
+++ Assets/Helpers/Multilanguage.cs
@@ -3,20 +3,36 @@ using System.Collections.Generic;
 
 namespace Helpers
 {
-    public class MultiLang<T>
+    public class MultiLang
     {
-        private readonly Dictionary<string, T> _langSet = new Dictionary<string, T>();
+        private readonly Dictionary<string, Dictionary<Language, string>>_langSet = new Dictionary<string, Dictionary<Language, string>>();
         
-        public MultiLang(T de, T en)
+        public MultiLang()
         {
-            _langSet.Add("de", de);
-            _langSet.Add("en", en);
+            
         }
-        
 
-        public T GetLang(Language lang)
+        public void AddSingle(string key, Dictionary<Language, string> value)
         {
-            return _langSet[lang == Language.De ? "de" : "en"];
+            _langSet.Add(key, value);
+        }
+
+        public void AddMultiple(string[] keys, Dictionary<Language, string>[] values)
+        {
+            if (keys.Length != values.Length)
+            {
+                throw new Exception("Keys and Values must have the same length");
+            }
+
+            for (int i = 0; i < keys.Length; i++)
+            {
+                _langSet.Add(keys[i], values[i]);
+            }
+        }
+
+        public string Get(string key, Language lang)
+        {
+            return _langSet[key][lang];
         }
     }
     
