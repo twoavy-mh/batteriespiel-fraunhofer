9d0e74e7ef6a97d45bda2d492e91d50c2031d574
Manuel Herr <mh@2av.de>
2024-02-01 17:04:33 +0100

    added 3d room control

--- Assets/Scripts/3D-Viewer/RotateModel.cs
+++ Assets/Scripts/3D-Viewer/RotateModel.cs
@@ -6,28 +6,31 @@ using UnityEngine;
 public class RotateModel : MonoBehaviour
 {
     public float mouseRotationSpeed = 10f;
+    public float mouseScrollSpeed = 1f;
     public float touchRotationSpeed = 0.5f;
     
-    public Boolean rotateXAxis = true;
-    public Boolean rotateYAxis = false;
-    
+    public GameObject cameraCenter;
+    public GameObject cameraRail;
     public Camera cam;
     private void OnMouseDrag()
     {
         float rotationX = Input.GetAxis("Mouse X") * mouseRotationSpeed;
         float rotationY = Input.GetAxis("Mouse Y") * mouseRotationSpeed;
         
-        Vector3 right = Vector3.Cross(cam.transform.up, transform.position - cam.transform.position);
-        Vector3 up = Vector3.Cross(transform.position - cam.transform.position, right);
+        float cameraCenterRotationY = cameraCenter.transform.localEulerAngles.y + rotationX;
+        float cameraRailRotationX =  Mathf.Clamp(cameraRail.transform.localEulerAngles.x - rotationY, 5f, 45f);
 
-        if (rotateYAxis)
-        {
-            transform.rotation = Quaternion.AngleAxis(-rotationX, up) * transform.rotation;
-        }
+        cameraCenter.transform.localEulerAngles = new Vector3(0f,cameraCenterRotationY,0f); 
+        cameraRail.transform.localEulerAngles = new Vector3(cameraRailRotationX,0f, 0f); 
+    }
 
-        if (rotateXAxis)
+    void OnGUI()
+    {
+        if (Input.mouseScrollDelta.y != 0)
         {
-            transform.rotation = Quaternion.AngleAxis(-rotationY, right) * transform.rotation;
+            float scrollY = Input.mouseScrollDelta.y * mouseScrollSpeed;
+            float cameraZoomPositionZ = Mathf.Clamp(cam.transform.localPosition.z - scrollY, -10f, -2f);
+            cam.transform.localPosition = new Vector3(0f,0f,cameraZoomPositionZ);
         }
     }
 
@@ -37,8 +40,13 @@ public class RotateModel : MonoBehaviour
         if (Input.touchCount > 0)
         {
             Touch touch = Input.GetTouch(0);
-            float deltaY = touch.deltaPosition.y;
-            transform.Rotate(transform.rotation.x, transform.rotation.y + deltaY, transform.rotation.z);
+            float deltaY = touch.deltaPosition.y * touchRotationSpeed;
+            float deltaX = touch.deltaPosition.x * touchRotationSpeed;
+            float cameraCenterRotationY = cameraCenter.transform.localEulerAngles.y + deltaX;
+            float cameraRailRotationX =  Mathf.Clamp(cameraRail.transform.localEulerAngles.x - deltaY, 5f, 45f);
+
+            cameraCenter.transform.localEulerAngles = new Vector3(0f,cameraCenterRotationY,0f); 
+            cameraRail.transform.localEulerAngles = new Vector3(cameraRailRotationX,0f, 0f); 
         }
     }
 }
