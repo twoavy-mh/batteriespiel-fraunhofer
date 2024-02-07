8b80179fee7f73434716c49dbc6f4c1df39104e4
Nils Blessing <nb@2av.de>
2024-02-07 12:07:56 +0100

    rectsizer

--- Assets/Scripts/RectSizer.cs
+++ Assets/Scripts/RectSizer.cs
@@ -1,5 +1,6 @@
 using System;
 using Helpers;
+using Nobi.UiRoundedCorners;
 using UnityEngine;
 
 public class RectSizer : MonoBehaviour
@@ -11,14 +12,24 @@ public class RectSizer : MonoBehaviour
     public float xDiff;
     public float yDiff;
     public float zDiff;
+
+    public bool adjustRoundedCorners = false;
     
     private void Start()
     {
+        RectTransform rt = GetComponent<RectTransform>();
         if (Utility.GetDevice() == Device.Mobile)
         {
-            RectTransform rt = GetComponent<RectTransform>();
             rt.sizeDelta = new Vector2(widthScreendesign * Settings.RESIZE_FACTOR, heightScreendesign * Settings.RESIZE_FACTOR);
             rt.localPosition = new Vector3(rt.localPosition.x + xDiff, rt.localPosition.y + yDiff, rt.localPosition.z + zDiff);
+        }
+
+        if (!adjustRoundedCorners) return;
+        ImageWithRoundedCorners i = GetComponent<ImageWithRoundedCorners>();
+        if (i != null)
+        {
+            i.radius = rt.sizeDelta.x / 2;
+            i.Refresh();
         }
     }
 }
