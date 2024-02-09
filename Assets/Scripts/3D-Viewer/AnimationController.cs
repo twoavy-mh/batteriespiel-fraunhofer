c7ab2b6613a2e5f7827e5e7e17f7843d32eb93e8
Manuel Herr <mh@2av.de>
2024-02-09 10:48:20 +0100

    finalized ar/3d mode

--- Assets/Scripts/3D-Viewer/AnimationController.cs
+++ Assets/Scripts/3D-Viewer/AnimationController.cs
@@ -8,7 +8,11 @@ public class AnimationController : MonoBehaviour
     private Animator _animator;
     
     private GameObject _animateStartOpenGameObject;
+    private GameObject _disabledOpenButtonGameObject;
     private GameObject _animateStartCloseGameObject;
+    private GameObject _disabledCloseButtonGameObject;
+
+    private GameObject openCloseAnimationGameObject;
     
     // Start is called before the first frame update
     void Start()
@@ -16,12 +20,20 @@ public class AnimationController : MonoBehaviour
         _animator = GetComponent<Animator>();
         _animator.enabled = true;
         
-        FindHiddenOpenCloseContainer();
+        openCloseAnimationGameObject = FindHiddenOpenCloseContainer();
+        openCloseAnimationGameObject.SetActive(true);
         
-        _animateStartOpenGameObject = GameObject.Find("OpenButton"); 
-        _animateStartCloseGameObject = GameObject.Find("CloseButton"); 
+        _animateStartOpenGameObject = GameObject.Find("OpenActiveButton"); 
+        _animateStartCloseGameObject = GameObject.Find("CloseActiveButton"); 
         _animateStartOpenGameObject.GetComponent<Button>().onClick.AddListener(triggerAnimation);
         _animateStartCloseGameObject.GetComponent<Button>().onClick.AddListener(triggerAnimation);
+        
+        _disabledCloseButtonGameObject = GameObject.Find("CloseDisabledButton");
+        _disabledOpenButtonGameObject = GameObject.Find("OpenDisabledButton");
+        _animateStartCloseGameObject.SetActive(false);
+        _disabledOpenButtonGameObject.SetActive(false);
+        
+        LayoutRebuilder.ForceRebuildLayoutImmediate(openCloseAnimationGameObject.GetComponent<RectTransform>());
     }
 
     // Update is called once per frame
@@ -32,21 +44,35 @@ public class AnimationController : MonoBehaviour
         {
             _animator.ResetTrigger("open");
             _animator.SetTrigger("close");
+            _animateStartCloseGameObject.SetActive(false);
+            _animateStartOpenGameObject.SetActive(true);
+            _disabledOpenButtonGameObject.SetActive(false);
+            _disabledCloseButtonGameObject.SetActive(true);
         }
         else
         {
             _animator.ResetTrigger("close");
             _animator.SetTrigger("open");
+            _animateStartCloseGameObject.SetActive(true);
+            _animateStartOpenGameObject.SetActive(false);
+            _disabledOpenButtonGameObject.SetActive(true);
+            _disabledCloseButtonGameObject.SetActive(false);
         }
+        
+        LayoutRebuilder.ForceRebuildLayoutImmediate(openCloseAnimationGameObject.GetComponent<RectTransform>());
     }
 
-    private void FindHiddenOpenCloseContainer()
+    private GameObject FindHiddenOpenCloseContainer()
     {
+        GameObject resultGameObject = null;
+        
         foreach (GameObject go in Resources.FindObjectsOfTypeAll(typeof(GameObject)) as GameObject[])
         {
             Debug.Log(go.name);
             if (go.name == "OpenCloseAnimation")
-                go.SetActive(true);
+                resultGameObject = go;
         }
+
+        return resultGameObject;
     }
 }
