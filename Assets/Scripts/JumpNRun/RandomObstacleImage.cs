2b15a32a3f9024743da908180464dfc33f66e592
Nils Blessing <nb@2av.de>
2024-05-03 16:08:06 +0200

    glitch over boxes

--- Assets/Scripts/JumpNRun/RandomObstacleImage.cs
+++ Assets/Scripts/JumpNRun/RandomObstacleImage.cs
@@ -43,7 +43,7 @@ public class RandomObstacleImage : MonoBehaviour
             GetComponent<PolygonCollider2D>().SetPath(0, points.ToArray());
             
             
-            GameObject second = new GameObject();
+            /*GameObject second = new GameObject();
             second.transform.SetParent(transform.parent.parent);
             second.transform.position = transform.position;
             second.AddComponent<SpriteRenderer>().sprite = GetComponent<SpriteRenderer>().sprite;
@@ -51,7 +51,7 @@ public class RandomObstacleImage : MonoBehaviour
             List<Vector2> secondPoints = new List<Vector2>();
             GetComponent<SpriteRenderer>().sprite.GetPhysicsShape(1, secondPoints);
             secondPoly.SetPath(0, secondPoints.ToArray());
-            second.tag = "Floor";
+            second.tag = "Floor";*/
         }
         else
         {
